01402317d9db21d04a5a0b84b223ea62be125e66
sbravindran03 <ravindran5211@gmail.com>
2024-08-25 16:52:45 +0530

    commit

---
+++ JavaScAdvanced_Concepts/tasks/task3.js
@@ -0,0 +1,8 @@
+// JSON Conversion: Convert a JavaScript object to JSON format and log it to the console.
+const user={
+    name:'john',
+    age:21,
+    city:'NYK'
+};
+const newjson=JSON.stringify(user);
+console.log(newjson);
